e12b39f1d42fb12c2ccaaa0cf4412e4946aebda1
MariusJM <mariusjm.code@gmail.com>
2024-02-04 00:54:08 +0200

    more experiments

--- 01_Complete Intro to Web Development, v3/03_js_experiments/experiments.js
+++ 01_Complete Intro to Web Development, v3/03_js_experiments/experiments.js
@@ -1,1 +1,54 @@
 console.log("Hello from experiments!")
+const monthlyRent = 600;
+const yerlyRent = monthlyRent * 12;
+console.log(yerlyRent)
+
+console.log("Hello, World!");
+console.log("Single ' inside \" \" is ok");
+console.log("use backslash to escape \\ you'll be fine");
+const program = "Python";
+console.log(`Backticks is the same as fString in ${program}`);
+
+
+const firstName = "Marius";
+const lastName = "Michailovskis";
+const sentence = "Hello " + firstName + " " + lastName + "! How are you?";
+const sentenceWithTemplate = `Hello ${firstName} ${lastName}! How are you?`;
+console.log(sentence);
+console.log(sentenceWithTemplate);
+
+let isItTrue = true;
+let isItFalse = false;
+console.log(`Is it True? ${isItTrue}`);
+console.log(isItTrue);
+console.log(`Is it False? ${isItFalse}`);
+console.log(isItFalse);
+
+const isScyBlue = true;
+
+if (isScyBlue){
+    console.log("Yes")
+    console.log("The sky is blue.");
+} else{
+    console.log("The sky is not Blue.");
+}
+console.log("Equality - try using tripple equals all the time")
+console.log(2 + 2 == "4");
+console.log(2 + 2 === "4");
+console.log(2 + 2 == 4);
+console.log(2 + 2 === 4);
+console.log(2 + 2 == 4.0);
+console.log(2 + 2 === 4.0);
+
+
+console.log("--------------------------------------------------------------");
+
+const friendAtParty = 10;
+if (friendAtParty === 0){
+    console.log("No friends at the party");
+} else if (friendAtParty <= 4){
+    console.log("LAN party");
+} else {
+    console.log("DISCO party");
+}
+// you can skip last else block
